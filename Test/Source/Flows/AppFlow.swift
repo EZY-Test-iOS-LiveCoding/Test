bbd985b305f1ee5a652ea607e960efb5e64ceb52
baekteun <baegteun@gmail.com>
2021-11-13 20:29:37 +0900

    create - splash Step

---
+++ Test/Source/Flows/AppFlow.swift
@@ -0,0 +1,8 @@
+//
+//  AppFlow.swift
+//  Test
+//
+//  Created by baegteun on 2021/11/13.
+//
+
+import Foundation
---
+++ Test/Source/Flows/Step/HanStep.swift
@@ -0,0 +1,19 @@
+//
+//  HanStep.swift
+//  Test
+//
+//  Created by baegteun on 2021/11/13.
+//
+
+import RxFlow
+
+enum HanStep: Step{
+    // Global
+    case dismiss
+    
+    // Main
+    case MainIsRequired
+    
+    //Location
+    case LocationIsRequired
+}
